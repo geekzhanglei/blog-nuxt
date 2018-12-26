a8df928809e5c0e2e6a8145037748656942406e4
zhanglei25 <zhanglei25@le.com>
2018-12-26 18:58:00 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+
+export default {
+    components: {
+        // Logo
+    }
+};
+</script>
+
+<style>
+</style>
